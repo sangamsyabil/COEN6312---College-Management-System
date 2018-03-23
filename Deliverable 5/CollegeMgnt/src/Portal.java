9e7e6a8f1bc732a22ff873881b6f569e0ee553e6
Hem Raj Regmi <sangamsyabil@gmail.com>
2018-03-23 11:38:53 -0400

    Sample code generation
    I have created one sample class diagram and generated the Java code for that.

---
+++ Deliverable 5/CollegeMgnt/src/Portal.java
@@ -0,0 +1,47 @@
+
+
+public class Portal {
+
+	/**
+	 * 
+	 */
+	private String userName;
+	/**
+	 * 
+	 */
+	private String password;
+	/**
+	 * Getter of userName
+	 */
+	public String getUserName() {
+	 	 return userName; 
+	}
+	/**
+	 * Setter of userName
+	 */
+	public void setUserName(String userName) { 
+		 this.userName = userName; 
+	}
+	/**
+	 * Getter of password
+	 */
+	public String getPassword() {
+	 	 return password; 
+	}
+	/**
+	 * Setter of password
+	 */
+	public void setPassword(String password) { 
+		 this.password = password; 
+	}
+	/**
+	 * 
+	 * @param getInfo 
+	 * @return 
+	 */
+	public String getDetails(String getInfo) { 
+		// TODO Auto-generated method
+		return null;
+	 } 
+
+}
